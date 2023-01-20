1e0e93d46380b68acea5a8d36087e6d74fc4453d
Anton Popenkov <popenkovaa@yandex.ru>
2023-01-20 08:10:15 +0300

    lesson 1.2 webpack config decomposition added

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,23 @@
 import webpack from 'webpack';
-import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
-import path from "path";
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths } from './config/build/types/config';
+import path from 'path';
+
+// выбираем нужный конфиг снаружи, снизу описаны параметры
 
 const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-}
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  build: path.resolve(__dirname, 'build'),
+  html: path.resolve(__dirname, 'public', 'index.html'),
+};
 
 const mode = 'development';
 const isDev = mode === 'development';
 
 const config: webpack.Configuration = buildWebpackConfig({
-    mode: 'development',
-    paths,
-    isDev,
-})
+  mode: 'development',
+  paths,
+  isDev,
+});
 
 export default config;
