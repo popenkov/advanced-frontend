baf6226726ae4d8cceda9e90331a2455fa0192e5
Anton Popenkov <popenkovaa@yandex.ru>
2023-01-21 14:58:55 +0300

    1.4 react and scss added

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,27 @@
-import webpack from 'webpack';
+import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-  // порядок лоадеров имеет значение, поэтому лучше выносить так в переменные
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  };
 
-  return [typescriptLoader];
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from js strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ]
+    }
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader,
+    ]
 }
