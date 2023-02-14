05246d750108dd7a987a134d868268cfb8364960
Anton Popenkov <popenkovaa@yandex.ru>
2023-02-14 11:06:57 +0300

    analyzer added, tests added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {someFn} from './test'
+import { someFn } from './test';
 
-someFn(123)
+someFn(123);
 
-document.body.innerHTML = `<div style="background: red">HELLO WORLD!</div>`
+document.body.innerHTML = '<div style="background: red">HELLO WORLD!</div>';
