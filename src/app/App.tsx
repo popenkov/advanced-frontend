cf0c28619556650fffbbb625b7e80022085013c9
a.popenkov <a.popenkov@thecoders.ru>
2023-04-12 14:19:22 +0300

    .browserslistrc added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,25 +8,25 @@ import { Navbar } from '@/widgets/Navbar';
 import { Sidebar } from '@/widgets/Sidebar';
 
 function App() {
-  const { theme } = useTheme();
-  const dispatch = useDispatch();
-  const inited = useSelector(getUserInited);
+    const { theme } = useTheme();
+    const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
 
-  useEffect(() => {
-    dispatch(userActions.initAuthData());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(userActions.initAuthData());
+    }, [dispatch]);
 
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          {inited && <AppRouter />}
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    {inited && <AppRouter />}
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 }
 
 export default App;
